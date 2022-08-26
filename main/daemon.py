1c884914b907ae51d57c000fffed8e8561123716
Mattias Dahlgren <mattias.dahlgren@leovegas.com>
2022-08-26 09:01:58 +0200

    Lint

--- main/daemon.py
+++ main/daemon.py
@@ -1,28 +1,43 @@
-import requests
+'''
+  The Daemon module
+'''
 import time
 import logging
-import sys
 import requests
 
 class Daemon():
+  '''
+  Instanciates a daemon for handling the requests.
+  '''
     def __init__(self, target, interval, is_https):
+      '''
+      Init function.
+      '''
         self.target = target
         self.interval = interval
         self.is_https = is_https
-    
+
     def daemonize(self):
         success = 0
         failure = 0
         while self.target is not None:
             #try:
-            logging.debug("Firing of request to {}".format(self.target))
+            logging.debug(f"Firing of request to {self.target}")
             r = requests.get(self.target)
             if r.status_code >= 200 and r.status_code < 300:
                 success += 1
-                requests.post('http://127.0.0.1:5000/stat', json={"target": self.target, "status": "up", "successes": success, "failures": failure})
+                requests.post('http://127.0.0.1:5000/stat',
+                json={"target": self.target,
+                "status": "up",
+                "successes": success,
+                "failures": failure})
             else:
                 failure += 1
-                requests.post('http://127.0.0.1:5000/stat', json={"target": self.target, "status": "down", "successes": success, "failures": failure})
+                requests.post('http://127.0.0.1:5000/stat',
+                json={"target": self.target,
+                "status": "down",
+                "successes": success,
+                "failures": failure})
             logging.debug("Request fired, sleeping for interval")
             time.sleep(self.interval)
           #except:
