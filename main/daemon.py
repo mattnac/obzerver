f5a5f6bc7c37c21878cc2ed6b7e6b93fd3fd03ab
Mattias Dahlgren <mattias.dahlgren@leovegas.com>
2022-08-26 08:54:56 +0200

    Lint

--- main/daemon.py
+++ main/daemon.py
@@ -5,26 +5,26 @@ import sys
 import requests
 
 class Daemon():
-  def __init__(self, target, interval, is_https):
-    self.target = target
-    self.interval = interval
-    self.is_https = is_https
-  
-  def daemonize(self):
-    success = 0
-    failure = 0
-    while self.target is not None:
-      #try:
-      logging.debug("Firing of request to {}".format(self.target))
-      r = requests.get(self.target)
-      if r.status_code >= 200 and r.status_code < 300:
-        success += 1
-        requests.post('http://127.0.0.1:5000/stat', json={"target": self.target, "status": "up", "successes": success, "failures": failure})
-      else:
-        failure += 1
-        requests.post('http://127.0.0.1:5000/stat', json={"target": self.target, "status": "down", "successes": success, "failures": failure})
-      logging.debug("Request fired, sleeping for interval")
-      time.sleep(self.interval)
-      #except:
-        #logging.error("Something went wrong with sending the request.")
-        #sys.exit(1)
+    def __init__(self, target, interval, is_https):
+        self.target = target
+        self.interval = interval
+        self.is_https = is_https
+    
+    def daemonize(self):
+        success = 0
+        failure = 0
+        while self.target is not None:
+            #try:
+            logging.debug("Firing of request to {}".format(self.target))
+            r = requests.get(self.target)
+            if r.status_code >= 200 and r.status_code < 300:
+                success += 1
+                requests.post('http://127.0.0.1:5000/stat', json={"target": self.target, "status": "up", "successes": success, "failures": failure})
+            else:
+                failure += 1
+                requests.post('http://127.0.0.1:5000/stat', json={"target": self.target, "status": "down", "successes": success, "failures": failure})
+            logging.debug("Request fired, sleeping for interval")
+            time.sleep(self.interval)
+          #except:
+            #logging.error("Something went wrong with sending the request.")
+            #sys.exit(1)
--- main/main.py
+++ main/main.py
@@ -1,3 +1,6 @@
+'''
+  Main module
+'''
 import os
 import urlformat
 import daemon
@@ -9,55 +12,54 @@ import json
 import time
 
 def main():    
-  target = ""
-  interval = 0
-  is_https = ""
+    target = ""
+    interval = 0
+    is_https = ""
 
-  if target == None or interval == None or is_https == None:
-    logging.error("Missing environment variables.")
+    if target is None or interval is None or is_https is None:
+        logging.error("Missing environment variables.")
 
-  os.environ['FLASK_APP'] = 'rest.py'
+    os.environ['FLASK_APP'] = 'rest.py'
 
-  logging.info("Starting REST API")
-  rest_int = threading.Thread(target=rest.start_server, daemon=True).start()
-  while True:
-    curr_queue = list(poll_queue())
-    logging.info(f"Queue is: {curr_queue}")
-    while len(curr_queue) > 0:
-      item = curr_queue.pop()
-      target, interval = item["target"], item["interval"]
-      is_https = True if "true" or "TRUE" in is_https else False
-      logging.info(f"Starting job for {target}")
-      job = threading.Thread(target=run, args=(target, interval, is_https)).start()
-      logging.info(f"queue length is {len(curr_queue)}")
-      if len(curr_queue) == 0:
-        requests.put('http://127.0.0.1:5000/clear-queue')
+    logging.info("Starting REST API")
+    rest_int = threading.Thread(target=rest.start_server, daemon=True).start()
+    while True:
+        curr_queue = list(poll_queue())
+        logging.info(f"Queue is: {curr_queue}")
+        while len(curr_queue) > 0:
+            item = curr_queue.pop()
+            target, interval = item["target"], item["interval"]
+            is_https = True if "true" or "TRUE" in is_https else False
+            logging.info(f"Starting job for {target}")
+            job = threading.Thread(target=run, args=(target, interval, is_https)).start()
+            logging.info(f"queue length is {len(curr_queue)}")
+            if len(curr_queue) == 0:
+              requests.put('http://127.0.0.1:5000/clear-queue')
 
-    time.sleep(5)
+      time.sleep(5)
 
 def run(target, interval, is_https):
-  logging.debug("In run function.")
-  # is_https = True if "true" or "TRUE" in is_https else False
-  # if "true" or "TRUE" in is_https:
-  #   is_https = True
-  # else:
-  #   is_https = False
+    logging.debug("In run function.")
+    # is_https = True if "true" or "TRUE" in is_https else False
+    # if "true" or "TRUE" in is_https:
+    #   is_https = True
+    # else:
+    #   is_https = False
 
-  run_url = urlformat.urlformat(target, is_https)
-  client = create_client(run_url, interval, is_https)
-  job = client.daemonize()
-  
-  return(job)
+    run_url = urlformat.urlformat(target, is_https)
+    client = create_client(run_url, interval, is_https)
+    job = client.daemonize()
+    return job
 
 def create_client(run_url, interval, is_https):
-  logging.debug("Creating client")
-  run_client = daemon.Daemon(run_url, interval, is_https)
-  return run_client
+    logging.debug("Creating client")
+    run_client = daemon.Daemon(run_url, interval, is_https)
+    return run_client
 
 def poll_queue():
-  job_queue = requests.get('http://127.0.0.1:5000/jobs')
-  return(job_queue.json()[0])
+    job_queue = requests.get('http://127.0.0.1:5000/jobs')
+    return job_queue.json()[0]
 
 if __name__ == "__main__":
-  logging.basicConfig(level=logging.INFO)
-  main()
+    logging.basicConfig(level=logging.INFO)
+    main()
